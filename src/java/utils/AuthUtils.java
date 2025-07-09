d0e85d5b39862be227aae87ad241d263b74c400f
Nhan <nminhnhan503@gmail.com>
2025-07-09 13:40:07 +0700

    Added useful utils

---
+++ src/java/utils/AuthUtils.java
@@ -0,0 +1,47 @@
+package utils;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import model.UserDTO;
+
+public class AuthUtils {
+    public static UserDTO getCurrentUser(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        if(session!=null){
+            return (UserDTO) session.getAttribute("user");
+        }
+        return null;
+    }
+    
+    public static boolean isLoggedIn(HttpServletRequest request){
+        return getCurrentUser(request)!=null;
+    }
+    
+    public static boolean hasRole(HttpServletRequest request, String role){
+        UserDTO user = getCurrentUser(request);
+        if(user!=null){
+            return user.getRoleID().equals(role);
+        }
+        return false;
+    }
+    
+    public static boolean isAdmin(HttpServletRequest request){
+        return hasRole(request, "AD");
+    }
+    
+    public static boolean isManager(HttpServletRequest request){
+        return hasRole(request, "MA");
+    }
+    
+    public static boolean isUser(HttpServletRequest request){
+        return hasRole(request, "MB");
+    }
+    
+    public static String getLoginURL(){
+        return "MainController";
+    }
+    
+    public static String getAccessDeniedMessage(String action){
+        return "You can not access to "+action+". Please contact administrator.";
+    }
+}
---
+++ src/java/utils/DbUtils.java
@@ -0,0 +1,34 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sun.security.pkcs11.Secmod;
+
+public class DbUtils {
+
+    private static final String DB_NAME = "PRJ301_PROJECT1";
+    private static final String DB_USER_NAME = "SA";
+    private static final String DB_PASSWORD = "12345";
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Connection conn = null;
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=" + DB_NAME;
+        conn = DriverManager.getConnection(url, DB_USER_NAME, DB_PASSWORD);
+        return conn;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            System.out.println(DbUtils.getConnection());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DbUtils.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DbUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
